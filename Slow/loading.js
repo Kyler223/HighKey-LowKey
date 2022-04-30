3de476cf60a380ca1d08346bb9f2508d769f41e2
RomeTwinn <rometwinn@gmail.com>
2022-04-30 16:04:21 -0500

    improvements to slow version

--- Slow/loading.js
+++ Slow/loading.js
@@ -1,8 +1,26 @@
-var timeToLoad = 2000;
-var pixelsToLoad = 3;
+var images = document.querySelectorAll('img');
+var i = 0;
+var pictureTime = 0;
+var nextPictureTime = 10;
+var pixelsToLoad = 1;
+var backgroundAlpha = 0;
 
-setInterval(function() {
-    pixelsToLoad = pixelsToLoad + 3;
-    document.documentElement.style.setProperty(
-        '--loading-image-height', pixelsToLoad + 'px');
-}, timeToLoad);
+function load() {
+    if(i === pictureTime) {
+        pixelsToLoad = pixelsToLoad + 3;
+        document.documentElement.style.setProperty(
+            '--loading-image-height', pixelsToLoad + 'px');
+        console.log(i);
+        pictureTime = pictureTime + nextPictureTime;
+    }
+    if(backgroundAlpha < 1) {
+        document.documentElement.style.setProperty(
+            '--pink-backgrond', `rgba(244, 197, 205, ${backgroundAlpha})`);
+            backgroundAlpha = backgroundAlpha + .1;
+    }
+
+    i++;
+    window.requestAnimationFrame(load);
+}
+
+window.requestAnimationFrame(load);
