0029c36a937c6736270b4ba68705b7cef392c522
Kyler <kyler@kylernelson.dev>
2022-05-02 15:21:05 -0500

    using clip-path: inset instead of height

--- Slow/loading.js
+++ Slow/loading.js
@@ -1,22 +1,26 @@
 var images = document.querySelectorAll('img');
 var i = 0;
 var pictureTime = 0;
-var nextPictureTime = 10;
-var pixelsToLoad = 1;
+var nextPictureTime = 40;
+var pixelsToLoad = 100;
 var backgroundAlpha = 0;
+var backgroundAlphaTime = 0;
 
 function load() {
     if(i === pictureTime) {
-        pixelsToLoad = pixelsToLoad + 3;
+        pixelsToLoad = pixelsToLoad - 1;
         document.documentElement.style.setProperty(
-            '--loading-image-height', pixelsToLoad + 'px');
+            '--loading-image-height', pixelsToLoad + '%');
         console.log(i);
         pictureTime = pictureTime + nextPictureTime;
     }
     if(backgroundAlpha < 1) {
-        document.documentElement.style.setProperty(
-            '--pink-backgrond', `rgba(244, 197, 205, ${backgroundAlpha})`);
-            backgroundAlpha = backgroundAlpha + .1;
+        if(i === backgroundAlphaTime) {
+            document.documentElement.style.setProperty(
+                '--pink-backgrond', `rgba(244, 197, 205, ${backgroundAlpha})`);
+                backgroundAlpha = backgroundAlpha + .15;
+                backgroundAlphaTime = i + 300;
+        }
     }
 
     i++;
