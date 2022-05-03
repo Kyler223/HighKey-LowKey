d7a016470b6a89ab4055e2566aedc01f67f2dada
Kyler <kyler@kylernelson.dev>
2022-05-03 13:27:32 -0500

    slow meduim fast loading implemented

--- Slow/loading.js
+++ Slow/loading.js
@@ -1,18 +1,60 @@
 var images = document.querySelectorAll('img');
 var i = 0;
-var pictureTime = 0;
-var nextPictureTime = 40;
-var pixelsToLoad = 98;
-var backgroundAlpha = 0.1;
+//on going time
+var slowPictureTime = 0;
+var meduimPictureTime = 0;
+var fastPictureTime = 0;
+//current percent cropped
+var slowLoadPercent = 98;
+var meduimLoadPercent = 98;
+var fastLoadPercent = 98;
+//when it will crop again
+var slowNext = 100;
+var meduimNext = 50;
+var fastNext = 25;
+
+var backgroundAlpha = 0;
 var backgroundAlphaTime = 0;
 
+//sets images if they should load slow, meduim, or fast
+var imagesForLoop = 1;
+images.forEach(image => {
+    switch(imagesForLoop) {
+        case 1:
+            image.classList.add("slow-loading");
+            imagesForLoop++
+            break;
+        case 2:
+            image.classList.add("meduim-loading");
+            imagesForLoop++
+            break;
+        case 3:
+            image.classList.add("fast-loading");
+            imagesForLoop = 1;
+            break;
+    }
+});
+
+console.log(images);
+
 function load() {
-    if(i === pictureTime) {
-        pixelsToLoad = pixelsToLoad - 1;
+    if(i === slowPictureTime) {
+        slowLoadPercent = slowLoadPercent - 1;
         document.documentElement.style.setProperty(
-            '--loading-image-height', pixelsToLoad + '%');
-        // console.log(i);
-        pictureTime = pictureTime + nextPictureTime;
+            '--slow-loading-image-height', `${slowLoadPercent}%`);
+        slowPictureTime = slowPictureTime + slowNext;
+    }
+    if(i === meduimPictureTime) {
+        meduimLoadPercent = meduimLoadPercent - 1;
+        document.documentElement.style.setProperty(
+            '--meduim-loading-image-height', `${meduimLoadPercent}%`);
+        slowPictureTime = slowPictureTime + meduimNext;
+    }
+    if(i === fastPictureTime) {
+        fastLoadPercent = fastLoadPercent - 1;
+        document.documentElement.style.setProperty(
+            '--fast-loading-image-height', `${fastLoadPercent}%`);
+        fastPictureTime = fastPictureTime + fastNext;
     }
     if(backgroundAlpha < 1) {
         if(i === backgroundAlphaTime) {
