b18d39518dc61bb8ae6053dca8f9a58e85e2b061
RomeTwinn <rometwinn@gmail.com>
2022-05-02 19:55:38 -0500

    added loading icon + minor improvments

--- Slow/loading.js
+++ Slow/loading.js
@@ -3,7 +3,7 @@ var i = 0;
 var pictureTime = 0;
 var nextPictureTime = 40;
 var pixelsToLoad = 100;
-var backgroundAlpha = 0;
+var backgroundAlpha = 0.1;
 var backgroundAlphaTime = 0;
 
 function load() {
@@ -11,15 +11,17 @@ function load() {
         pixelsToLoad = pixelsToLoad - 1;
         document.documentElement.style.setProperty(
             '--loading-image-height', pixelsToLoad + '%');
-        console.log(i);
+        // console.log(i);
         pictureTime = pictureTime + nextPictureTime;
     }
     if(backgroundAlpha < 1) {
         if(i === backgroundAlphaTime) {
             document.documentElement.style.setProperty(
                 '--pink-backgrond', `rgba(244, 197, 205, ${backgroundAlpha})`);
-                backgroundAlpha = backgroundAlpha + .15;
-                backgroundAlphaTime = i + 300;
+            backgroundAlpha = backgroundAlpha + .2;
+            backgroundAlphaTime = i + 300;
+            console.log(`backgroundAlpha: ${backgroundAlpha}`);
+            // need to round to the nearest decimal
         }
     }
 
